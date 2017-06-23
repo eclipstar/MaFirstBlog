caa941f28b3384d23f0b084d4a85fb2eb81b09f4
eclipstar <rdmh_@hotmail.com>
2017-06-23 12:44:00 -0600

    im first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\central\programitas pacheco pacheco xd\sistemas\2017\django\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
